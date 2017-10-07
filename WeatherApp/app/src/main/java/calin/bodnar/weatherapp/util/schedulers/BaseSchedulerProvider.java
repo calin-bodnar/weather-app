18e430edfd95fa700726febbd95ef5ecba9592b5
Calin Bodnar <calin.bodnar@gmail.com>
2017-10-07 16:30:42 +0300

    Add schedulers and utils packages from Android Open Source Project

---
+++ WeatherApp/app/src/main/java/calin/bodnar/weatherapp/util/schedulers/BaseSchedulerProvider.java
@@ -0,0 +1,20 @@
+package calin.bodnar.weatherapp.util.schedulers;
+
+import android.support.annotation.NonNull;
+
+import io.reactivex.Scheduler;
+
+/**
+ * Allow providing different types of {@link Scheduler}s.
+ */
+public interface BaseSchedulerProvider {
+
+    @NonNull
+    Scheduler computation();
+
+    @NonNull
+    Scheduler io();
+
+    @NonNull
+    Scheduler ui();
+}
---
+++ WeatherApp/app/src/main/java/calin/bodnar/weatherapp/util/schedulers/ImmediateSchedulerProvider.java
@@ -0,0 +1,34 @@
+package calin.bodnar.weatherapp.util.schedulers;
+
+import android.support.annotation.NonNull;
+
+import calin.bodnar.weatherapp.util.schedulers.BaseSchedulerProvider;
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * Implementation of the {@link BaseSchedulerProvider} making all {@link Scheduler}s execute
+ * synchronously so we can easily run assertions in our tests.
+ * <p>
+ * To achieve this, we are using the {@link io.reactivex.internal.schedulers.TrampolineScheduler} from the {@link Schedulers} class.
+ */
+public class ImmediateSchedulerProvider implements BaseSchedulerProvider {
+
+    @NonNull
+    @Override
+    public Scheduler computation() {
+        return Schedulers.trampoline();
+    }
+
+    @NonNull
+    @Override
+    public Scheduler io() {
+        return Schedulers.trampoline();
+    }
+
+    @NonNull
+    @Override
+    public Scheduler ui() {
+        return Schedulers.trampoline();
+    }
+}
